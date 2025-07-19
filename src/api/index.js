866395253367e0f9bde7beb844abd0ab9bdc3d37
Richard <fennix530@gmail.com>
2025-07-19 19:02:12 -0300

    Nuevo ajuste para redeploy

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
 import serverless from 'serverless-http';
-import app from '../src/app.js';
+import app from './app.js'; // ✅ usa './app.js' porque está en la raíz
 
 export const handler = serverless(app);
