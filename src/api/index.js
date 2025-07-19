332b8cd100e5a803933ddf32efd01be592b17218
Richard <fennix530@gmail.com>
2025-07-19 18:55:05 -0300

    Nuevo ajuste para redeploy

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,4 @@
 import serverless from 'serverless-http';
 import app from '../src/app.js';
 
-export default function handler(req, res) {
-  res.send('Servidor vivo desde Vercel ✅');
-}
-
 export const handler = serverless(app);
-
