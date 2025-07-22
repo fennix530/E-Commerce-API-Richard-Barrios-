be408b3a27833830ce005cbcd82204762645cdb5
Richard <fennix530@gmail.com>
2025-07-22 20:48:38 -0300

    Fix: importación faltante de cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import productRoutes from './routes/productRoutes.js';
 import authRoutes from './routes/auth.routes.js';
 import db from './config/firebase.js';
