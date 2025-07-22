ff48e5f340914a7c5d397804ccaf07bcf2ba9a1d
Richard <fennix530@gmail.com>
2025-07-22 18:38:35 -0300

    Funcionado todo ok local

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import productRoutes from './routes/productroutes.js';
+import productRoutes from './routes/productRoutes.js';
 import authRoutes from './routes/auth.routes.js';
 
 dotenv.config();
